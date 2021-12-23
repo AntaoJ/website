44245e22f94633e623a7312d11370fdb60177da2
davidlgabriel <david.g.leal00@gmail.com>
2021-12-23 21:28:57 +0000

    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,80 @@
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
-
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+          <section class="col-lg-7 connectedSortable">
+            <!-- Custom tabs (Charts with tabs)-->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">
+                  <i class="fas fa-chart-pie mr-1"></i>
+                  Recomende amigos e ganhe!
+                </h3>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content p-0">
+                  <!--  Sales -->
+                  <div class="chart tab-pane active" id="revenue-chart"
+                       style="position: relative; height: 200px;">
+                     
+                   </div>
+                </div>
+              </div><!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </section>
+           <!-- Left col -->
+           <section class="col-lg-7 connectedSortable">
+            <!-- Custom tabs (Charts with tabs)-->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">
+                  <i class="fas fa-chart-pie mr-1"></i>
+                 Pretende vender um imóvel connosco?
+                </h3>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content p-0">
+                  <!--  Sales -->
+                  <div class="chart tab-pane active" id="revenue-chart"
+                       style="position: relative; height: 200px;">
+                     
+                   </div>
+                </div>
+              </div><!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </section>
+        </section>
+        <!-- Left col -->
+        <section class="col-lg-7 connectedSortable">
+         <!-- Custom tabs (Charts with tabs)-->
+         <div class="card">
+           <div class="card-header">
+             <h3 class="card-title">
+               <i class="fas fa-chart-pie mr-1"></i>
+              Pretende comprar um imóvel connosco?
+             </h3>
+           </div><!-- /.card-header -->
+           <div class="card-body">
+             <div class="tab-content p-0">
+               <!--  Sales -->
+               <div class="chart tab-pane active" id="revenue-chart"
+                    style="position: relative; height: 200px;">
+                  
+                </div>
+             </div>
+           </div><!-- /.card-body -->
+         </div>
+         <!-- /.card -->
+       </section>
+          <!-- /.Left col -->
+      </div><!-- /.container-fluid -->
+    </section>
 
 
 </div>
