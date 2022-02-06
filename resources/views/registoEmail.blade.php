f09951d78c4427df93146c669790c23fdae3746b
davidlgabriel <david.g.leal00@gmail.com>
2022-02-06 16:16:14 +0000

    email

--- resources/views/registoEmail.blade.php
+++ resources/views/registoEmail.blade.php
@@ -3,20 +3,20 @@
 <p>O seu Link de convite:</p> <a href="https://20mediar.pt/register?invite=<?php $data['invite'] ?>">https://20mediar.pt/register?invite={{$data['invite']}}</a>
 
 
-<p>A 20Mediar dá-lhe as mais cordiais boas vindas!</p><br>
+<p>A <b>20Mediar</b> dá-lhe as mais cordiais boas vindas!</p><br>
 
-<p>Confirmamos o seu registo nesta plataforma de negócios. A partir de agora participa de importantes vantagens e benefícios. </p><br>
+<p>Confirmamos o seu registo nesta plataforma de negócios. A partir de agora participa de importantes <b>vantagens e benefícios</b>. </p><br>
 <p>Por exemplo:</p>
-<li>Como cliente, ao vender o seu imóvel, será reembolsado de parte da comissão imobiliária acordada entre as partes.</li>
-<li>Ao enviar o link de convite ao seu círculo de amigos será recompensado(a) com uma percentagem da comissão originada por cada transação realizada por eles.</li>
+<li>Como cliente, ao vender o seu imóvel, será reembolsado de parte da <b>comissão imobiliária</b> acordada entre as partes.</li>
+<li>Ao enviar o <b>link de convite</b> ao seu <b>círculo de amigos</b> será recompensado(a) com uma <b>percentagem da comissão</b> originada por cada transação realizada por eles.</li>
 
-<p>Saiba como beneficiar-se do marketing de influência!</p>
+<p>Saiba como <b>beneficiar-se</b> do <b>marketing de influência</b>!</p>
 
 <p><b>Com a 20Mediar todos ganham!</b></p><br>
 
 <p>Atenciosamente</p>
 <br>
-<p>20Mediar Unipessoal Lda <br>
+<p></b>20Mediar Unipessoal Lda</b> <br>
     Lic. AMI nº 19601</p>
 <a href="tel:+351939786872">+351939786872</a><br>
 <a href="mailto:geral@20mediar.com" >geral@20mediar.com</a><br>
