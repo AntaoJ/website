9c012bc1a68dd68c70ca9335f1180896dcb9da37
davidlgabriel <david.g.leal00@gmail.com>
2022-02-07 01:52:22 +0000

    d

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'reset' => 'A sua password foi recuperada!',
+    'sent' => 'Enviamos o link de recuperação para o seu email!',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'user' => "Coloque um email válido!",
 
 ];
