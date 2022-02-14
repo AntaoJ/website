309626fd007b70ee136bfd0e73ad7a9cbf898582
davidlgabriel <david.g.leal00@gmail.com>
2022-02-14 19:37:55 +0000

    d

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -97,15 +97,16 @@
                                         &emsp;Como cliente, ao vender o seu imóvel, será reembolsado de parte da
                                         <b>comissão
                                             imobiliária</b>
-                                        acordada entre as partes. <br><br>
-                                        &emsp;Se for comprador, receberá igualmente parte da comissão
-                                        imobiliária<br><br>
-                                    &emsp;Ao enviar o <b>link de convite</b> ao seu <b>círculo de amigos</b> será
-                                    recompensado(a) com uma
-                                    <b>percentagem da comissão</b> originada por cada transação realizada por eles.
-                                    <br><br>
-                                    &emsp;Saiba como <b>beneficiar-se</b> do <b>marketing de influência</b>!<br>
-                                    <br><br>
+                                        acordada entre as partes. 
+                                        Se for comprador, receberá igualmente parte da comissão imobiliária
+                                        <br><br>
+
+                                        &emsp;Ao enviar o <b>link de convite</b> ao seu <b>círculo de amigos</b> será
+                                        recompensado(a) com uma
+                                        <b>percentagem da comissão</b> originada por cada transação realizada por eles.
+                                        <br><br>
+                                        &emsp;Saiba como <b>beneficiar-se</b> do <b>marketing de influência</b>!<br>
+                                        <br><br>
                                     </p>
                                 </div>
                             </div>
