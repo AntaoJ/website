490712a37ef81f647d68af8309efcf19f21109ac
davidlgabriel <david.g.leal00@gmail.com>
2022-02-14 19:55:28 +0000

    h

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
     }
     public function faq()
     {
-        $faqs = Faq::all()->sortByDesc("id");;
+        $faqs = Faq::all()->sortByDesc("titulo");;
         return view('faq',[ 'faqs' => $faqs ]);
     }
     public function onde()
